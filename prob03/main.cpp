2270bd0f63e420cdbac0f37cc3dd8515913fde22
JimmyC1204 <jimmyc1204@cs104u-3d.acad.fullerton.edu>
2019-09-07 14:35:54 -0700

    created a function that converts inches into a person's height using measurements of height and feet

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,18 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+
+int main()
+{
+  int inches, height1, height2;
+
+  //get user input
+  std::cout << "Please enter the person's height in inches:" << '\n';
+  std::cin >> inches;
+
+  //convert
+  height1 = inches / 12;
+  height2 = inches % 12;
+
+  std::cout << "That person is " << "" << height1 << "'" << height2 << "\"" <<'\n';
+  return 0;
+}
